686213484673e6489c7152a3273d6988237f9bad
Thomas Rausch <thomas@rauschig.org>
2015-11-09 15:49:13 +0100

    Add common IntelliJ IDEA config files

---
+++ .IntelliJIdea/config/fileTemplates/UtilClass.java
@@ -0,0 +1,10 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+public final class ${NAME} {
+
+    private ${NAME}() {
+        // util class
+    }
+    
+    
+}
